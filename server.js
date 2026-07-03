785129b642939f7f538dad7a2428349b2a0f5822
tatrizjayareja-hq <tatrizjayareja@gmail.com>
2026-07-04 02:13:55 +0700

    dpcicil4

--- server.js
+++ server.js
@@ -2478,7 +2478,7 @@ app.get('/input-kerja-admin', isAdmin, async (req, res) => {
             FROM po_utama u
             WHERE u.tenant_id = $1 
             AND (
-                u.status = 'Produksi', 'DP/Cicil'
+                p.status IN ('Produksi', 'DP/CICIL')
                 OR 
                 (u.status = 'CMT' AND EXISTS (
                     SELECT 1 FROM po_detail d 
